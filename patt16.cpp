e21fefaab8637c800409190ef40427b464d5da19
Iram Hundekari <100361020+IramHundekari@users.noreply.github.com>
2022-10-22 21:28:53 +0530

    Add files via upload

---
+++ patt16.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int row=1;
+    while(row<=n)
+    {
+        //print space
+        int space=n-row;
+        while(space)
+        {
+           cout<<" ";
+           space--;
+        }
+        //print 1st triangle
+        int col=1;
+        while(col<=row)
+        {
+            cout<<"*";
+            col++;
+        }
+         //print 2nd triangle
+         int start=row-1;
+         while(start)
+         {
+             cout<<"*";
+             start--;
+         }
+       cout<<endl;
+       row++;
+   
+    }
+}
