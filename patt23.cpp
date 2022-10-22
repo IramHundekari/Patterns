520e3d90136c4b84dc1a76c5420de31b23d1382e
Iram Hundekari <100361020+IramHundekari@users.noreply.github.com>
2022-10-22 21:11:59 +0530

    Add files via upload
             ****
           ****
         ****
       ****

---
+++ patt23.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int n;
+     cin>>n;
+     int r=1;
+     while(r<=n)
+     {
+        int sp=n-r;
+        while(sp){
+            cout<<" ";
+            sp--;
+        }
+        int c=1;
+        while(c<=n){
+            if(r==1 && r==n && c==1 && c==n){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+            c++;
+        }
+        cout<<endl;
+        r++;
+     }
+
+    return 0;
+}
