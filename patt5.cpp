a972519e8a6bbb4520734e3e582cb795280ae82d
Iram Hundekari <100361020+IramHundekari@users.noreply.github.com>
2022-10-23 13:34:49 +0530

    Add files via upload

---
+++ patt5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r,c,n;
+    cin>>n;
+    for(r=n;r>=1;r--){
+        for(c=1;c<=r;c++){
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+    int r1,c1;
+    for(r1=2;r1<=n;r1++){
+        for(c1=1;c1<=r1;c1++){
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
